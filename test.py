dbc6a04837cb584b913205abf8c0222e0b85e134
sachin <sachinkumar69@gmail.com>
2024-02-18 10:20:39 -0800

    modiy test.py

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 import subprocess
 
+#subprocess.run (['cp', '-p', '/etc/passwd', '/tmp/passwd'])
 #subprocess.run ('rm -f /tmp/passwd', shell=True)
 
-print (subprocess.__run__)
+print('Enter the source path and destination path of which you want to copy')
+sourcePath = input('Enter the source path with file name: ')
+destPath = input('Enter the destinatin path with file name: ')
+
+subprocess.run(['cp', '-p', sourcePath, destPath])
